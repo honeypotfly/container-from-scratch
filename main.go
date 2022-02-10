b2ce8c29b0c95608be2ef9c663065e23da556695
honeypotfly <honeypotfly@protonmail.com>
2022-02-10 06:38:52 -0500

    a fat commit, what this does is basically create a way to make syscalls while also storing stin, stout, and stderr. It is a bit more special though, we specify a namespace on the hostname. This way, if we run a shell, it should inherit its own hostname and therefore have that degree of seperation from the host machine

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+    "syscall"
+)
+
+// docker               run image <cmd> <params>
+// go run main.go       run       <cmd> <params>
+
+
+func main() {
+    switch os.Args[1] {
+    case "run":
+        run()
+    case "child":
+        child()
+    default:
+        panic ("bad command")
+    }
+}
+
+
+// This one is going to create the system, it is going to run *itself*. We need this in order to set the hostname inside the container
+func run() {
+    fmt.Printf("Running %v\n", os.Args[2:])
+
+    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+    // Need the below routes so that we can see things happen
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr= os.Stderr
+
+    // Now, we finally set the namespace
+    cmd.SysProcAttr = &syscall.SysProcAttr {
+        Cloneflags: syscall.CLONE_NEWUTS, // NEW_UTS is a new unix timesharing system. It just gives a hostname to the container.
+    }
+    cmd.Run()
+
+}
+
+func child() {
+    fmt.Printf("Running %v\n", os.Args[2:])
+
+    // Now, we can set the hostname
+    syscall.Sethostname([]byte("container"))
+
+    cmd := exec.Command(os.Args[2], os.Args[3:]...)
+    // Need the below routes so that we can see things happen
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr= os.Stderr
+
+   cmd.Run()
+
+}
+func must (err error) {
+    if err != nil {
+        panic(err)
+    }
+}
